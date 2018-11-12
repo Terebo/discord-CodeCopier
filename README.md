83f9a39a809c15c893080ba62f7791ec91c88a38
Marf <git@marfprojects.nl>
2018-11-12 14:55:51 +0100

    Added the main file, nothing fancy. really cringy.
    + Added index.js
    + Added package.json
    * modified README.md
    
    Signed-off-by: Marf <git@marfprojects.nl>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # discord-moneyprinter
-A shitty JaVaScRiPT that sends some discord rich-presence that shows that you're printing money
+A shitty JaVaScRiPT that sends some discord rich-presence that shows that you're printing money on discord as a joke.
+
+Due that people are asking me on discord how i did it, this is how i did it.
---
+++ index.js
@@ -0,0 +1,24 @@
+const client = require('discord-rich-presence')('470242275671277578');
+let currentmonney = 0;
+let timesprinted = 0;
+
+
+
+
+function update() {
+currentmonney = currentmonney + 500;
+timesprinted = timesprinted + 1;
+console.log(currentmonney);
+client.updatePresence({
+  state: 'Printed: ' + timesprinted,
+  details: '€ ' + currentmonney + ",-",
+  startTimestamp: Date.now(),
+  endTimestamp: 1541174400,
+  largeImageKey: 'printer',
+  largeImageText: "Printer",
+  smallImageKey: 'euro',
+  instance: true,
+});
+}
+
+setInterval(function(){ update() }, 1000);
