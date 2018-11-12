75a591e200277ece69f4289e5efc47f7962fa0d1
Marf <git@marfprojects.nl>
2018-11-12 15:00:27 +0100

    Cleaned it up a little.
    Signed-off-by: Marf <git@marfprojects.nl>

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const client = require('discord-rich-presence')('470242275671277578');
 let currentmonney = 0;
 let timesprinted = 0;
-
-
-
-
-function update() {
-currentmonney = currentmonney + 500;
-timesprinted = timesprinted + 1;
-console.log(currentmonney);
-client.updatePresence({
-  state: 'Printed: ' + timesprinted,
-  details: '€ ' + currentmonney + ",-",
-  startTimestamp: Date.now(),
-  endTimestamp: 1541174400,
-  largeImageKey: 'printer',
-  largeImageText: "Printer",
-  smallImageKey: 'euro',
-  instance: true,
-});
-}
-
-setInterval(function(){ update() }, 1000);
+setInterval(function(){ 
+  currentmonney = currentmonney + 500;
+  timesprinted = timesprinted + 1;
+  console.log(currentmonney);
+  client.updatePresence({
+    state: 'Printed: ' + timesprinted,
+    details: '€ ' + currentmonney + ",-",
+    startTimestamp: Date.now(),
+    endTimestamp: 1541174400,
+    largeImageKey: 'printer',
+    largeImageText: "Printer",
+    smallImageKey: 'euro',
+    instance: true,
+  });
+}, 1000);
